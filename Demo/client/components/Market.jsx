3a89a2c4c4e6aeadb932731df4e517375cc00e2c
Matthew Garza <mattg614@gmail.com>
2022-09-24 15:13:46 -0400

    getDispatch method retrieves the dispatched reducers and stores in fileNode Co-authored-by: Kara C. <kkchis@users.noreply.github.com>

--- Demo/client/components/Market.jsx
+++ Demo/client/components/Market.jsx
@@ -12,12 +12,10 @@
 import React from 'react';
 
 
+
 const Market = props => {
   const { addCard, deleteCard } = props;
 
-  console.log(addCard);
-  console.log(props.totalcards);
-  console.log('in Market, here is props:', props);
 
 
   return (
--- main/src/FileNode.js
+++ main/src/FileNode.js
@@ -18,6 +18,7 @@ class FileNode {
   }
 
   getRenders() {}
+
   getSelectedState(astBody) {
     this.selected = [];
     astBody.forEach((node) => {
@@ -57,7 +58,75 @@ class FileNode {
       }
     });
   }
-  getDispatched(ast) {}
+  getDispatched(astBody) {
+    this.dispatched = [];
+    astBody.forEach((node) => {
+      if (node.type === 'VariableDeclaration') {
+        const declarations = node.declarations;
+        //declarations is the entire MarketContainer function, including "const MarketsContainer"
+        declarations.forEach((declaration) => {
+          if (declaration.init.type === 'ArrowFunctionExpression') {
+            //ArrowFunctionExpression is the body of MarketsContainer (everything after the equals sign after MarketsContainer)
+            const ArrowFuncBlock = declaration.init.body.body;
+            //ArrowFuncBlock is the array of blocks (?) within MarketsContainer
+            if (ArrowFuncBlock) {
+              let useDispatchLabel;
+              ArrowFuncBlock.forEach((blockElement) => {
+                if (blockElement.type === 'VariableDeclaration') {
+                  //declarationsArray is all code/data about one variable declaration within MarketsContainer
+                  const declarationsArray = blockElement.declarations;
+                  declarationsArray.forEach((element) => {
+                    if (
+                      useDispatchLabel === undefined &&
+                      element?.init?.callee?.name === 'useDispatch'
+                    ) {
+                      //useDispatchLabel is what useDispatch gets saved to, so for us it's "dispatch"
+                      useDispatchLabel = element.id.name;
+                    } else {
+                      if (element.init.type === 'ArrowFunctionExpression') {
+                        //innerArrowFuncBlock is everything after the arrow within an ArrowFunctionExpression variable declaration within MarketsContainer - for example, within handleDeleteCard, it's everything after the arrow.
+                        const innerArrowFuncBlock = element.init.body.body;
+                        innerArrowFuncBlock.forEach((element) => {
+                          if (
+                            useDispatchLabel !== undefined &&
+                            element.type === 'ExpressionStatement' &&
+                            element.expression.callee.name === useDispatchLabel
+                          ) {
+                            //  console.log(
+                            //   'args name',
+                            //   //we can just grab arguments[0] because we know that there will only ever be one argument to useDispatch
+                            //   element.expression.arguments[0].callee.name
+                            // );
+                            this.dispatched.push(
+                              element.expression.arguments[0].callee.name
+                            );
+                          }
+                        });
+                      }
+                    }
+                  });
+                }
+                if (
+                  useDispatchLabel !== undefined &&
+                  blockElement.type === 'ExpressionStatement' &&
+                  blockElement.expression.callee.name === useDispatchLabel
+                ) {
+                  // console.log(
+                  // 'args name',
+                  //we can just grab arguments[0] because we know that there will only ever be one argument to useDispatch
+                  // blockElement.expression.arguments[0].callee.name
+                  // );
+                  this.dispatched.push(
+                    blockElement.expression.arguments[0].callee.name
+                  );
+                }
+              });
+            }
+          }
+        });
+      }
+    });
+  }
 }
 
 export default FileNode;
