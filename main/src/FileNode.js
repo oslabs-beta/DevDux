15eb5d4bfedcd1b62fde572ba628507f94b4d156
Hina Khalid <k.hinaa87@gmail.com>
2022-09-25 20:22:12 -0400

    able to pull rendered components

--- main/src/FileNode.js
+++ main/src/FileNode.js
@@ -1,4 +1,5 @@
 import * as babelParser from '@babel/parser';
+import { Console } from 'console';
 import * as fs from 'fs';
 import path from 'path';
 
@@ -12,12 +13,12 @@ class FileNode {
     // this.fileImports = getFileImports(this.ast);
     // this.selectedState = getSelectedState(this.ast);
     // this.dispatchedReducers = getDispatched(this.ast);
+    this.imports = [];
   }
   getType(ast) {
     this.type = 'slice file';
   }
 
-  getRenders() {}
   getSelectedState(astBody) {
     this.selected = [];
     astBody.forEach((node) => {
@@ -54,10 +55,114 @@ class FileNode {
             }
           }
         });
+        //console.log('declaration ------->', declarations)
       }
     });
   }
-  getDispatched(ast) {}
+
+  getDispatched(ast) { }
+
+
+  // getRenderComponents() {
+  //   //console.log("----> checking the body for each node: ", this.astBody);
+  //   const jsxElements = [];
+  //   console.log('CHECKING THE TOKENS.', this.astTokens);
+  //   this.astBody.forEach((node) => {
+  //     if (node.type === 'VariableDeclaration') {
+  //       const declaration = node.declarations;
+  //       //console.log("DECLARATION -------->", declaration);
+
+  //       declaration.forEach((declaration) => {
+  //         if (declaration.init.type === 'ArrowFunctionExpression') {
+  //           // after ArrowFunctionExpression node, go into the body
+  //           const initBody = declaration.init.body;
+  //           console.log("----initBody--->", initBody.body)
+
+  //           initBody.body.forEach((node) => {
+  //             if (node.type === 'ReturnStatement') {
+
+  //               const arg = node.argument;
+  //               //console.log("ARG: ", arg);
+  //               // console.log(arg.children[1]);
+  //               arg.children[1].children.forEach((child) => {
+  //                 if (child.type === 'JSXElement') {
+  //                   console.log('child ---------->', child);
+  //                 }
+  //               })
+  //               // arg.forEach((node) => {
+  //               //   if (node.type === 'JSXOpeningElement') {
+  //               //     console.log('JSXOpeningElement node -->', node)
+  //               //   }
+  //               //})
+  //             }
+  //           })
+
+  //         }
+  //       })
+  //     }
+  //   })
+  // }
+
+  getRenderComponents() {
+    //console.log("--IMPORTS-->", this.imports)
+    const keyArray = this.imports.map((obj) => {
+      return Object.keys(obj)[0];
+    })
+    //console.log(keyArray);
+
+    const result = this.astTokens.slice().filter((token) => {
+      if (token.type.label === 'jsxName' && (token.value[0] === token.value[0].toUpperCase())) {
+        if (keyArray.includes(token.value)) {
+          return true;
+        }
+      }
+      return false;
+    })
+    //console.log(result); <--------------------------
+  }
+
+  getProps() {
+    console.log(this.astTokens);
+  }
+  //console.log("array------>", this.imports);
+
+  //this.astTokens.filter(token => {
+  // const arr = this.imports;
+  // if (arr.includes(token.value)) {
+  //   console.log('hi')
+  // }
+  //if (this.imports.includes) {
+
+  //}
+  //})
+
+  // this.astTokens.forEach((token) => {
+  //console.log("------CHECKING TOKEN--->", token);
+  //console.log("imports?", this.imports);
+  //if (token.type.label === 'jsxName') {
+  //console.log("------CHECKING TOKEN--->", token); // token.value 
+  //console.log("------CHECKING TOKEN VALUE--->", token.value);
+  //console.log("imports?", this.imports);
+  // this.imports.forEach((element) => { // element is each obj in the import array
+  //   if (element[token.value]) {
+
+  //   }
+  // })
+
+  //jsxList.push(token);
+  //console.log("VALUE --> ", token.value[0])
+  //if (token.value[0] === token.value[0].toUpperCase()) {
+  //console.log('Token value', token.value);
+  //}
+  //console.log("printing first letter??", token.type.label[0]);
+  // check to see if the first letter in the label is uppercase
+  //}
+  // })
+  //console.log('array--->', jsxList);
+  // this.astBody.forEach((node) => {
+  //   console.log("checking the NODE--->", node);
+  // })
 }
+
 
 export default FileNode;
--- main/src/parse.js
+++ main/src/parse.js
@@ -4,86 +4,13 @@ import path from 'path';
 import FileNode from './FileNode.js';
 // console.log('test');
 
-<<<<<<< HEAD
-
 // const test = fs.readFileSync(
-//   '/Users/joshuamiller/Codesmith/DevDux/Demo/client/containers/MainContainer.jsx',
+//   '/Users/hinakhalid/CodeSmith/OSP/ospDemo/DevDux/Demo/client/containers/MarketsContainer.jsx',
 //   'utf-8',
 //   (err, data) => {
 //     if (err) console.log(err);
 //   }
 // );
-
-const test = fs.readFileSync(
-=======
->>>>>>> 7f7094f8 (Co-authored-by: hina-khalid <hina-khalid@users.noreply.github.com>)
-
-const test = fs.readFileSync(
-
-  '/Users/joshuamiller/Codesmith/DevDux/Demo/client/containers/MainContainer.jsx',
-  'utf-8',
-  (err, data) => {
-    if (err) console.log(err);
-  }
-);
-
-<<<<<<< HEAD
-=======
-// const test = fs.readFileSync(
-//   '/Users/joshuamiller/Codesmith/DevDux/Demo/client/containers/MainContainer.jsx',
-//   'utf-8',
-//   (err, data) => {
-//     if (err) console.log(err);
-//   }
-// );
-
->>>>>>> 7f7094f8 (Co-authored-by: hina-khalid <hina-khalid@users.noreply.github.com>)
-// console.log('working');
-// //console.log(data);
-// const ast = babelParser.parse(data);
-// console.log(ast);
-// return ast;
-//   }
-// );
-
-// const ast = babelParser.parse(test, {
-//   sourceType: 'module',
-//   tokens: true,
-//   plugins: ['jsx', 'typescript'],
-// });
-
-// ast.program.body.forEach((node) => {
-//   if (node.type === 'VariableDeclaration') {
-//     const declarations = node.declarations;
-
-//     declarations.forEach((declaration) => {
-//       if (declaration.init.type === 'ArrowFunctionExpression') {
-//         const ArrowFuncBlock = declaration.init.body.body;
-
-//         ArrowFuncBlock.forEach((blockElement) => {
-//           if (blockElement.type === 'VariableDeclaration') {
-//             const declarationsArray = blockElement.declarations;
-
-//             declarationsArray.forEach((element) => {
-//               if (element?.init?.callee?.name === 'useSelector') {
-//                 const variableLabel = element.id.name;
-//                 const useSelectorArguments = element.init.arguments;
-
-//                 useSelectorArguments.forEach((argument) => {
-//                   if (argument.type === 'ArrowFunctionExpression'){
-//                     const reducerName = argument.body.object.property.name;
-//                     const stateName = argument.body.property.name;
-
-//                   }
-//                 })
-//               }
-//             });
-//           }
-//         });
-//       }
-//     });
-//   }
-// });
 
 const fileData = {};
 
@@ -112,22 +39,14 @@ const getImports = (filePath) => {
             path.parse(currentFile).dir,
             node.source.value
           );
+          let importName;
+          node.specifiers.forEach((specifier) => {
+            //console.log(specifier);
+            fileData[baseName].imports.push({ [specifier.local.name]: importFile })
+          })
 
-<<<<<<< HEAD
-          //console.log({currentFile}, node.source.value)
-          if (fileData[importFile] === undefined) {
-
-          const baseName = path.parse(importFile).base;
-          if (fileData[baseName] === undefined) {
-
-=======
-          const baseName = path.parse(importFile).base;
-          if (fileData[baseName] === undefined) {
-
-          //console.log({currentFile}, node.source.value)
-          if (fileData[importFile] === undefined) {
-
->>>>>>> 7f7094f8 (Co-authored-by: hina-khalid <hina-khalid@users.noreply.github.com>)
+          const importListBaseName = path.parse(importFile).base;
+          if (fileData[importListBaseName] === undefined) {
             importList.push(importFile);
           }
         }
@@ -136,101 +55,38 @@ const getImports = (filePath) => {
   }
 };
 
-
-<<<<<<< HEAD
-const fp = path.resolve('../../Demo/client/index.js');
-
-
-// fp prints ---> /Users/joshuamiller/Codesmith/DevDux/Demo/client/index.js
-
 const fp = path.resolve('../../Demo/client/App.jsx');
-
 getImports(fp);
-
-
-// getImports(fp);
-
-
-getImports(fp);
-
-=======
-const fp = path.resolve('../../Demo/client/App.jsx');
-
-const fp = path.resolve('../../Demo/client/index.js');
-// fp prints ---> /Users/joshuamiller/Codesmith/DevDux/Demo/client/index.js
-
-getImports(fp);
-
->>>>>>> 7f7094f8 (Co-authored-by: hina-khalid <hina-khalid@users.noreply.github.com>)
 // console.log(fileData);
 const buildClasses = (fD) => {
   for (const [file, node] of Object.entries(fD)) {
-    // console.log(file);
-    console.log(file);
+    //console.log(file);
     node.getSelectedState(node.astBody);
-    console.log(node.selected);
+    //console.log(node.selected);
+    // console.log(file);
+    //console.log(file, ': imports --->', node.imports);
+    // console.log("--->", node.imports[0])
   }
 };
-buildClasses(fileData);
-
-<<<<<<< HEAD
-// console.log(fileData['MarketCreator.jsx'].filePath);
-
-// const filesToVisit = [];
-
-const MarketsContainerNode = fileData['./MarketsContainer.jsx'];
-
-
-//console.log(fileData)
-console.log(fileData['./MarketsContainer.jsx'].astTokens === fileData['./Market.jsx'].astTokens);
-//console.log(fileData['../components/TotalsDisplay.jsx'].astTokens)
-
-const getRenders = (fileNode) => {
-  //console.log('in getRenders')
-  //console.log("--->", fileNode.astTokens)
-  fileNode.astTokens.forEach( (token, i) => {
-    //console.log("token type:", token.type);DY
-    // const currentToken = token
-    //console.log(i, token.value)
-    //if (token.type.label.includes('JSX')) console.log(token);
-  })
-}
-//getRenders(MarketContainerNode);
-=======
 // console.log(fileData);
-// const filesToVisit = [];
+// console.log(buildClasses(fileData));
+buildClasses(fileData)
+const node = fileData['MarketsContainer.jsx'];
+node.getProps();
 
-const MarketsContainerNode = fileData['./MarketsContainer.jsx'];
-
-
-//console.log(fileData)
-console.log(fileData['./MarketsContainer.jsx'].astTokens === fileData['./Market.jsx'].astTokens);
-//console.log(fileData['../components/TotalsDisplay.jsx'].astTokens)
-
-const getRenders = (fileNode) => {
-  //console.log('in getRenders')
-  //console.log("--->", fileNode.astTokens)
-  fileNode.astTokens.forEach( (token, i) => {
-    //console.log("token type:", token.type);DY
-    // const currentToken = token
-    //console.log(i, token.value)
-    //if (token.type.label.includes('JSX')) console.log(token);
-  })
-}
-<<<<<<< HEAD
-
-getRenders(MarketsContainerNode);
->>>>>>> fb4ea6b8 (Co-authored-by: hina-khalid <hina-khalid@users.noreply.github.com>)
-=======
-//getRenders(MarketContainerNode);
->>>>>>> 7f7094f8 (Co-authored-by: hina-khalid <hina-khalid@users.noreply.github.com>)
-
-// console.log(
-//   ast.program.body.filter((node) => {
-//     if (node.type === 'ImportDeclaration') {
-//       console.log(node.source.value);
-//       return node.source.value === './containers/MainContainer.jsx';
-//     }
+// const getRenders = (fileNode) => {
+//   //console.log('in getRenders')
+//   //console.log("--->", fileNode.astTokens)
+//   fileNode.astTokens.forEach((token, i) => {
+//     //console.log("token type:", token.type);DY
+//     // const currentToken = token
+//     //console.log(i, token.value)
+//     //if (token.type.label.includes('JSX')) console.log(token);
 //   })
-// );
+// }
 
+
+// figure out which components are being rendered in each file
+// and which props are being passed in each file
+
+//buildClasses(fileData);
