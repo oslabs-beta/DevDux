5b0c40d360f54e3f92fba386b9c2e46e2863b4d3
Matthew Garza <mattg614@gmail.com>
2022-10-12 14:09:25 -0400

    Completed commenting and cleanup

--- devdux/src/extension.ts
+++ devdux/src/extension.ts
@@ -20,7 +20,6 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 
-	// vscode.window.registerTreeDataProvider('devdux-sidebar', new TreeDataProvider(context));
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "devdux" is now active!');
